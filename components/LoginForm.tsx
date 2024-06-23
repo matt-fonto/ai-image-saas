7a49726a896eb1c883fd6373a345a1ad191f194b
matt-fonto <mateus.fonto@gmail.com>
2024-06-23 11:09:11 +0200

    fix: build errors

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -129,7 +129,7 @@ export function LoginForm({ className }: ContactFormProps) {
 
         <div className="text-center text-sm text-gray-600">
           <p>
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link href="/signup" className="text-blue-600 hover:text-blue-700">
               Sign Up
             </Link>
